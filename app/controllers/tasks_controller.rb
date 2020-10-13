fdf6843669744f0478526f1972798e3de2912dfb
vagrant <vagrant@localhost.localdomain>
2020-10-13 08:27:15 +0000

    Finish:chapter4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
-class TasksController < ApplicationController
+class TasksController < ApplicationControllera
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-  	@tasks = Task.all
+  	@tasks = current_user.tasks.recent
   end
 
   def show
-  	@task = Task.find(params[:id])
+    # current_user.tasks.find(params[:id])
+  	# @task = Task.find(params[:id])
   end
 
   def new
@@ -12,33 +14,40 @@ class TasksController < ApplicationController
   end
 
   def create
-  	@task = Task.new(task_params)
+  	@task = current_user.tasks.new(task_params)
   	if @task.save
-  	  redirect_to tasks_url, notice: "タスク「#{task.name}」を保存しました。"
+  	  redirect_to @task, notice: "タスク「#{@task.name}」を保存しました。"
     else
       render :new
     end
   end
 
   def edit
-  	@task = Task.find(params[:id])
+    # current_user.tasks.find(params[:id])
+  	# @task = Task.find(params[:id])
   end
 
   def update
-  	task = Task.find(params[:id])
+    # current_user.tasks.find(params[:id])
+  	# task = Task.find(params[:id])
   	task.update!(task_params)
-  	redirect_to tasks_url, notice: "タスク 「#{task.name}」を更新しました。"
+  	redirect_to tasks_url, notice: "タスク 「#{@task.name}」を更新しました。"
   end
 
   def destroy
-  	task = Task.find(params[:id])
+    # current_user.tasks.find(params[:id])
+  	# task = Task.find(params[:id])
   	task.destroy
-  	redirect_to tasks_url, notice: "タスク 「#{task.name}」を削除しました。"
+  	redirect_to tasks_url, notice: "タスク 「#{@task.name}」を削除しました。"
   end
 
   private
 
   def task_params
   	params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
