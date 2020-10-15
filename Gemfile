90eed8d8f4f1a22890303f41bf4214ae11943e22
vagrant <vagrant@localhost.localdomain>
2020-10-15 03:06:44 +0000

    Finish:chapter4

--- Gemfile
+++ Gemfile
@@ -39,7 +39,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  # gem 'rspec-rails', '~> 3.7'
+  gem 'rspec-rails', '~> 3.7'
   gem 'factory_bot_rails', '~> 4.11'
 end
 
@@ -67,3 +67,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'slim-rails'
 gem 'html2slim'
 gem 'bootstrap'
+gem 'rails_autolink'
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-  	@tasks = Task.all
+  	@tasks = current_user.tasks.recent
   end
 
   def show
-  	@task = Task.find(params[:id])
+  	# @task = Task.find(params[:id])
+    # @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -12,33 +14,40 @@ class TasksController < ApplicationController
   end
 
   def create
-  	@task = Task.new(task_params)
+  	@task = Task.new(task_params.merge(user_id: current_user.id))
   	if @task.save
-  	  redirect_to tasks_url, notice: "タスク「#{task.name}」を保存しました。"
+  	  redirect_to @task, notice: "タスク「#{@task.name}」を保存しました。"
     else
       render :new
     end
   end
 
   def edit
-  	@task = Task.find(params[:id])
+  	# @task = Task.find(params[:id])
+    # @task = current_user.tasks.find(params[:id])
   end
 
   def update
-  	task = Task.find(params[:id])
-  	task.update!(task_params)
-  	redirect_to tasks_url, notice: "タスク 「#{task.name}」を更新しました。"
+  	# task = Task.find(params[:id])
+    # task = current_user.tasks.find(params[:id])
+  	@task.update!(task_params)
+  	redirect_to tasks_url, notice: "タスク 「#{@task.name}」を更新しました。"
   end
 
   def destroy
-  	task = Task.find(params[:id])
-  	task.destroy
-  	redirect_to tasks_url, notice: "タスク 「#{task.name}」を削除しました。"
+  	# task = Task.find(params[:id])
+    # task = current_user.tasks.find(params[:id])
+  	@task.destroy
+  	redirect_to tasks_url, notice: "タスク 「#{@task.name}」を削除しました。"
   end
 
   private
 
   def task_params
   	params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
