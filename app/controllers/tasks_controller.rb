ff8e30281daf5c8a1a5215f8f7512dcefe36c282
vagrant <vagrant@localhost.localdomain>
2020-10-15 08:51:00 +0000

    Finish:chapter7-1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,8 +13,19 @@ class TasksController < ApplicationController
   	@task = Task.new
   end
 
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
+  end
+
   def create
-  	@task = Task.new(task_params.merge(user_id: current_user.id))
+  	@task = current_user.tasks.new(task_params)
+
+    if params[:back].present?
+      render :new
+      return
+    end
+
   	if @task.save
   	  redirect_to @task, notice: "タスク「#{@task.name}」を保存しました。"
     else
